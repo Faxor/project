b8c6d04199fee0174f60ccf45ba8a6b97338f83c
Faxor <faxorgames@gmail.com>
2022-01-18 13:58:01 +0200

    work wersion 2

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -1,7 +1,8 @@
+from dataclasses import fields
 from rest_framework import serializers
 from blog.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ('id', 'title', 'author','excerpt','content','status')
