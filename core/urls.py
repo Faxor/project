0e21c5611d5e83d7761c40b0dc739ac2912d6e13
Faxor <faxorgames@gmail.com>
2022-01-23 15:49:41 +0200

    ADD authefication

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from argparse import Namespace
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'), name='blog'),
     path('api/', include('blog_api.urls'), name='blog_api'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
